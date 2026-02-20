72185c8de09e1f33936ad9a47177db637dc407f5
ilaria <bianchi.ilaria.studente@itispaleocapa.it>
2026-02-20 10:24:46 +0100

    struttura

--- BIANCHI.cpp
+++ BIANCHI.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 using namespace std;
 
+// struttura prodotto con nome, categoria, prezzo
+struct prodotto{
+	string nome;
+	string categoria;
+	float prezzo;
+};
 
-int main(int argc, char** argv) {
-
+int main(int argc, char** argv){
+	 
+	prodotto p;
+	prodotto supermercato[100];
 	
 	return 0;
 }
