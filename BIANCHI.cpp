229e71da37217b1fe2ee2c779ae55a40e6652527
ilaria <bianchi.ilaria.studente@itispaleocapa.it>
2026-02-20 10:32:23 +0100

    funzioni

--- BIANCHI.cpp
+++ BIANCHI.cpp
@@ -8,10 +8,36 @@ struct prodotto{
 	float prezzo;
 };
 
+// funzione aggiungi
+void aggiungi (prodotto p, prodotto v[], int &d){
+	
+}
+
+// funzione visualizza
+void visualizza (prodotto v[], int d){
+	
+}
+
+// funzione cerca
+int cerca (prodotto p, prodotto v[], int d){
+	
+}
+
+// funzione cancella
+bool cancella (prodotto p, prodotto v[], int d){
+	
+}
+
+//funzione modifica
+bool modifica (prodotto p, prodotto v[], int d){
+	
+}
+
 int main(int argc, char** argv){
 	 
 	prodotto p;
 	prodotto supermercato[100];
+	int d=sizeof(supermercato)/sizeof(supermercato[0]);
 	
 	return 0;
 }
