906805bec17204567022b81bb3fe02f86c6c0071
ilaria <bianchi.ilaria.studente@itispaleocapa.it>
2026-02-20 10:51:34 +0100

    menù

--- BIANCHI.cpp
+++ BIANCHI.cpp
@@ -1,29 +1,29 @@
 #include <iostream>
 using namespace std;
 
-// struttura prodotto con nome, categoria, prezzo
+//struttura prodotto con nome, categoria, prezzo
 struct prodotto{
 	string nome;
 	string categoria;
 	float prezzo;
 };
 
-// funzione aggiungi
+//funzione aggiungi
 void aggiungi (prodotto p, prodotto v[], int &d){
 	
 }
 
-// funzione visualizza
+//funzione visualizza
 void visualizza (prodotto v[], int d){
 	
 }
 
-// funzione cerca
+//funzione cerca
 int cerca (prodotto p, prodotto v[], int d){
 	
 }
 
-// funzione cancella
+//funzione cancella
 bool cancella (prodotto p, prodotto v[], int d){
 	
 }
@@ -37,7 +37,49 @@ int main(int argc, char** argv){
 	 
 	prodotto p;
 	prodotto supermercato[100];
+	//dimensione array
 	int d=sizeof(supermercato)/sizeof(supermercato[0]);
+	
+	//menù per chiamare le funzionalità del programma
+	int opzione;
+	do{
+		cout<<"1 - aggiungi \n2 - visualizza \n3 - cerca \n4 - cancella \n5 - modifica \n";
+		cout<<"inserisci l'opzione: ";
+		cin>>opzione;
+		
+		switch(opzione){
+			case 1:{
+				//inserimento
+				aggiungi(p, supermercato, d);
+			}
+			break;
+			
+			case 2:{
+				//visualizzazione
+				visualizza(supermercato, d);	
+			}
+			break;
+			
+			case 3:{
+				//cerca
+				int posizione=cerca(p, supermercato, d);	
+			}
+			break;
+			
+			case 4:{
+				//cancellazione
+				bool cancellazione=cancella(p, supermercato, d);	
+			}
+			break;
+			
+			case 5:{
+				//modifica
+				bool cambiamento=modifica(p, supermercato, d);	
+			}
+			break;
+		}
+		
+	}while(opzione!=0);
 	
 	return 0;
 }
